b7d342b55169fac223484d3a84c50870550ed6ff
RahulB <rahulbisht1012@gmail.com>
2024-01-05 10:39:47 +0530

    Add a New Operators Folder in the Repo
    I learnt About Python Operators. what are Operators , different type
    of operators provided by Python and work with them.
    
    Also used if else statement and used ternary Operator.
    the Syntax of `Ternary Operator`is a little bit differnt
    in  Python in comparesion of Other languages such as java
    c++ or even Javascript which is very similar to Python.
    
    Student - Rahul B
    Tutor - Dave gray [YouTube]

---
+++ Operators/If_Else.py
@@ -0,0 +1,20 @@
+print()
+
+val = 70
+
+if val > 70:
+    print("val is greater than 70")
+elif val == 70:
+    print("val is equal to 70")
+    print("Nice Buddy")
+else:
+    print("val is less than 70")
+
+
+# ! Ternary Operator
+print()
+print("   using Ternary Operators    ")
+print()
+print("Right On!") if val > 70 else print("Right Off!")
+
+print()
---
+++ Operators/Operator.py
@@ -0,0 +1,102 @@
+print()
+print("Lesson 2 : Python Operators")
+
+
+"""
+! What is Operator ??
+
+* operators are special symbols or keywords that perform operations on 
+* variables and values. They are used to manipulate data and perform 
+* various computations.
+
+! Python Supports a Variety of Operators
+
+* Arithmetic Operators
+* Assignment Operators
+* Comparison Operators
+* Logical Operators
+* Bitwise Operators
+
+"""
+
+
+# ! Assignment Operators.
+
+"""
+= (assign value)
++=, -=, *=, /=, %=, //=, **= (compound assignment operators)
+"""
+
+a = 10
+a += 4
+# print(a)
+
+
+# ! Arithmetic Operators.
+
+"""
++ (addition)
+- (subtraction)
+'*' (multiplication)
+/ (division)
+% (modulo, returns the remainder of the division)
+
+(floor division, returns the quotient without the remainder)
+
+** (exponentiation, raises a number to the power of another)
+"""
+
+# print(5 / 4)
+# print(5 // 4)
+# * Round Function()
+
+# print(round(5 / 4))
+
+# print(5 * 2)
+# print(5**2)  # give the Square
+
+# print(pow(5, 4))
+
+
+# ! String Concatenation :
+# * Concatenation is the process of joining two or more strings
+# * '+' Operator is used to concatenate strings in Python.
+
+
+# print("Rahul " + "B")
+
+
+# ! Comparison Operators
+
+"""
+`==` (equal to)
+`!=` (not equal to)
+`<`  (less than)
+`>`  (greater than)
+`<=` (less than or equal to)
+`>=` (greater than or equal to)
+"""
+
+# print(69 == 69)
+# print(34 > 45)
+
+#! Logical / Boolean Operators
+
+"""
+`and` (logical AND)
+`or` (logical OR)
+`not` (logical NOT)
+"""
+val1 = True
+val2 = False
+val3 = True
+
+print(not val1)
+
+print(val1 and val2)
+print(val1 and val3)
+
+print(val1 or val2)
+print(val2 or False)
+
+print()

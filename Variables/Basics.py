3d1f4b68ef6a33e933f53d417884fdeb63417553
RahulB <rahulbisht1012@gmail.com>
2024-01-05 09:50:59 +0530

    Add Folder called Variable in the Repo.
    Here i Learnt about what variables are.What are the Rules related to
    Python variable naming convention and much more.
    
    Student - Rahul B
    Tutor - Dave gray [Youtube]

---
+++ Variables/Basics.py
@@ -0,0 +1,37 @@
+print()
+print()
+print("Lesson 1 : Python Basics")
+
+"""
+^ Python 3.11.2
+
+* In this Lesson we are going to learn
+* About Python variables
+
+* Python is a high-level, interpreted, general-purpose programming language.
+* It is Not strictly typed and supports multiple programming paradigms.
+* variables are containers for storing data values.
+
+* Rules for naming a variable in Python are similar to any other 
+* Programming Language such as Java , C / C++ etc.
+
+* ' #' used for single line comment
+* ' ''' ''' ' used for multi-line comment is used for multi-line comment    
+
+
+"""
+
+line1 = "***********************************"
+line2 = "*                                 *"
+line3 = "*         Welcome! RahulB         *"
+
+
+print("")
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+
+print()
+print()
